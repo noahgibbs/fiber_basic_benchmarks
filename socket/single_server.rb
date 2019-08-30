d125a242d541e444c26f35615250758c7f92c17a
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-08-30 14:45:52 -0700

    Add single-process single-thread simple server

---
+++ socket/single_server.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require 'socket'
+
+RESPONSE_TEXT = "OK".freeze
+
+server = TCPServer.new('localhost', 9090)
+
+loop do
+    client = server.accept
+
+    query = client.read(6)   # Or sysread?
+    if query == "STATUS"
+      client.print RESPONSE_TEXT
+    else
+      STDERR.puts "READ gave bad result: #{query.inspect}, not STATUS"
+    end
+    client.close
+end
